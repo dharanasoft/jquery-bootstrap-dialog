3bbf746751c4c9afdb8f8dddb8b7a7c941c5eeb9
willyw <wwilimek@gmail.com>
2012-05-10 08:08:12 -0500

    updated to make changes so close doesnt remove but just hides. Added a hide_close option to hide the top right x in the window. Added a condition for options.content to use  if options.content is not defined

--- jquery.bootstrap.dialog.js
+++ jquery.bootstrap.dialog.js
@@ -1,14 +1,14 @@
 (function($){
   $.fn.bootstrap_dialog = function(options){
-    var title, content, ok, cancel, remove;
+    var title, content, ok, cancel, remove, hide_close;
     var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
  
     if(typeof options == "string"){
       switch(options){
         case "close": 
-          $(this).data("current_dialog").remove();
-          $("#modal_background").remove();
-          $("#modal").remove();
+          $(this).data("current_dialog").hide();
+          $("#modal_background").hide();
+          $("#modal").hide();
           break;
         case "destroy":
           $(this).data("current_dialog").remove();
@@ -21,6 +21,7 @@
     if(options) {
       title=options.title;
       content=options.content;
+      hide_close=options.hide_close;
       if(options.ok)
         ok=__bind(options.ok,this);
       if(options.cancel)
@@ -39,10 +40,18 @@
     current_dialog=$("<div class='modal'></div>").appendTo("body").attr("style","position:absolute; z-index: 25000; margin: 0");
     current_dialog.css("top",($(window).height()-current_dialog.height())/2);
     current_dialog.css("left",($(window).width()-current_dialog.width())/2);
-    $(this).data("current_dialog",current_dialog)
-    $("<div class='modal-header'/>").appendTo(current_dialog).append("<h3>"+title+"</h3");
-    console.log(current_dialog)   
-    $("<div class='modal-body'>").appendTo(current_dialog).append(content);
+    $(this).data("current_dialog",current_dialog);
+    if(options.hide_close) {
+      $("<div class='modal-header'/>").appendTo(current_dialog).append("<h3>"+title+"</h3");
+    } else {
+      $("<div class='modal-header'/>").appendTo(current_dialog).append("<h3>"+title+"</h3").append("<a href='#' class='close'>x</a>");
+    }
+    if(options.content) {
+      $("<div class='modal-body'>").appendTo(current_dialog).append(options.content);
+    } else {
+      $("<div class='modal-body'>").appendTo(current_dialog).append($(this));
+    }
+    console.log(current_dialog)
     if(ok || cancel || remove) {
       footer = $("<div class='modal-footer'>").appendTo(current_dialog);
       if(ok) {
@@ -51,7 +60,6 @@
       if(cancel) {
         $("<a href='#' class='btn'>Cancel</a>").appendTo(footer).click(function(){cancel();});
       }
-
       if(remove) {
         $("<a href='#' class='btn btn-danger'>Delete</a>").appendTo(footer).click(function(){remove();});
       }
